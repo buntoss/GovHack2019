e7e7c31dfd8d1bdef223546650b642bc0a3c1bb6
Tylor Bunting <buntoss@gmail.com>
2019-09-07 10:39:11 +1000

    progress on github pages index

--- ATO_Snapshot_table_7_analysis.R
+++ ATO_Snapshot_table_7_analysis.R
@@ -16,12 +16,16 @@ Data_1$ATO_Snapshot_table_7C <- read_csv("_data/ATO_Snapshot_table_7_data/ATO_Sn
 
 Data_2 <- Data_1
 
+if(exists("Analysis") != TRUE) {
+  Analysis <- list()
+}
+
 Data_2$ATO_Snapshot_table_7B_state_summary <- Data_2$ATO_Snapshot_table_7B %>%
   filter(`Top or bottom` == "Top") %>%
   group_by(State = `State/ Territory1`) %>%
-  summarise(`Individuals no.` = sum(`Individuals\nno.`),
-            `Average taxable income or loss` = sum(`Average taxable income or loss3\n$`),
-            `Median taxable income or loss` = sum(`Median taxable income or loss3\n$`),
+  summarise(`Individuals no.` = sum(`Individuals\r\nno.`),
+            `Average taxable income or loss` = sum(`Average taxable income or loss3\r\n$`),
+            `Median taxable income or loss` = sum(`Median taxable income or loss3\r\n$`),
             `Australian rank descending` = sum(`Australian rank5 descending`))
 
 # 3. TIDY DATA ------------------------------------------------------------
@@ -41,28 +45,34 @@ Data_3$ATO_Snapshot_table_7B_state_summary_gathered <- Data_3$ATO_Snapshot_table
 
 Data_4 <- Data_3
 
-Data_4$ATO_Snapshot_table_7B_state_avgTaxIncome_plot <- Data_4$ATO_Snapshot_table_7B_state_summary_gathered %>%
+if(exists("Analysis") != TRUE) {
+  Analysis <- list()
+}
+
+Analysis$Tables$ATO_Snapshot_table_7B_state_summary_gathered <- Data_4$ATO_Snapshot_table_7B_state_summary_gathered
+
+Analysis$Plots$ATO_Snapshot_table_7B_state_avgTaxIncome <- Analysis$Tables$ATO_Snapshot_table_7B_state_summary_gathered %>%
   filter(variable == "Average taxable income or loss") %>%
   ggplot(aes(x = State, y = value)) +
   geom_bar(stat = 'identity') +
   labs(title = "Analysis of top 10 post codes per state, based on average taxable income ", y = "Avg Taxable Income ($)") +
   theme(plot.title = element_text(hjust = 0.5))
 
-Data_4$ATO_Snapshot_table_7B_state_medTaxIncome_plot <- Data_4$ATO_Snapshot_table_7B_state_summary_gathered %>%
+Analysis$Plots$ATO_Snapshot_table_7B_state_medTaxIncome <- Analysis$Tables$ATO_Snapshot_table_7B_state_summary_gathered %>%
   filter(variable == "Median taxable income or loss") %>%
   ggplot(aes(x = State, y = value)) +
   geom_bar(stat = 'identity') +
   labs(title = "Analysis of top 10 post codes per state, based on average taxable income ", y = "Median Taxable Income ($)") +
   theme(plot.title = element_text(hjust = 0.5))
 
-Data_4$ATO_Snapshot_table_7B_state_AusRankDesc_plot <- Data_4$ATO_Snapshot_table_7B_state_summary_gathered %>%
+Analysis$Plots$ATO_Snapshot_table_7B_state_AusRankDesc <- Analysis$Tables$ATO_Snapshot_table_7B_state_summary_gathered %>%
   filter(variable == "Australian rank descending") %>%
   ggplot(aes(x = State, y = value)) +
   geom_bar(stat = 'identity') +
   labs(title = "Analysis of top 10 post codes per state, based on average taxable income ", y = "Aus Rank Desc") +
   theme(plot.title = element_text(hjust = 0.5))
 
-Data_4$ATO_Snapshot_table_7B_state_NumberOfIndividuals_plot <- Data_4$ATO_Snapshot_table_7B_state_summary_gathered %>%
+Analysis$Plots$ATO_Snapshot_table_7B_state_NumberOfIndividuals <- Analysis$Tables$ATO_Snapshot_table_7B_state_summary_gathered %>%
   filter(variable == "Individuals no.") %>%
   ggplot(aes(x = State, y = value)) +
   geom_bar(stat = 'identity') +
---
+++ DSS_Welfare_Payment_analysis.R
@@ -0,0 +1,54 @@
+# 0. SETUP ENVIRONMENT ----------------------------------------------------
+
+library("readr")
+library("tidyr")
+library("dplyr")
+library("ggplot2")
+
+# 1. GET DATA -------------------------------------------------------------
+
+Data_1 <- list()
+
+Data_1$DSS_Welfare_Payments_March2014 <- read_csv("_data/DSS_Welfare_Payments_March2014.csv")
+
+# 2. CLEAN DATA ------------------------------------------------------------
+
+Data_2 <- Data_1
+
+# 3. TIDY DATA ------------------------------------------------------------
+
+Data_3 <- Data_2
+
+
+# 4. VISUALISE DATA -------------------------------------------------------
+
+Data_4 <- Data_3
+
+if(exists("Analysis") != TRUE) {
+Analysis <- list()
+}
+
+Analysis$Tables$DSS_Welfare_Payments_State_Summary_YouthAllowance <- Data_4$DSS_Welfare_Payments_March2014 %>%
+  group_by(State = `state_of_commonwealth_electoral_`) %>%
+  summarise(Youth_Allowance_Payments = sum(`youth_allowance__student_`)) %>%
+  filter(State != c("Total", "Unknown")) 
+
+Analysis$Plots$DSS_Welfare_Payments_State_Summary_YouthAllowance <- Analysis$Tables$DSS_Welfare_Payments_State_Summary_YouthAllowance %>%
+  ggplot(aes(x = State, y = Youth_Allowance_Payments)) +
+  geom_bar(stat = 'identity') +
+  labs(title = "Analysis of Total Student Youth Allowance Welfare Payments by State in 2014 (Electorate Data)", y = "Total Payments ($)") +
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text.x = element_text(angle = 45, hjust = 1))
+
+Analysis$Tables$DSS_Welfare_Payments_State_Summary_AusStudy <- Data_4$DSS_Welfare_Payments_March2014 %>%
+  group_by(State = `state_of_commonwealth_electoral_`) %>%
+  summarise(Austudy_Payments = sum(austudy)) %>%
+  filter(State != c("Total", "Unknown"))
+
+Analysis$Plots$DSS_Welfare_Payments_State_Summary_AusStudy <- Analysis$Tables$DSS_Welfare_Payments_State_Summary_AusStudy %>%
+  ggplot(aes(x = State, y = Austudy_Payments)) +
+  geom_bar(stat = 'identity') +
+  labs(title = "Analysis of Total Austudy Welfare Payments by State in 2014 (Electorate Data)", y = "Total Payments ($)") +
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text.x = element_text(angle = 45, hjust = 1))
+
